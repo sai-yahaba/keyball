22d476888c176445bf732f7fccab58ceec8a43fc
sai-yahaba <sai.31.yahaba@gmail.com>
2024-09-09 14:13:09 +0900

    modify

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mykeymaps/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mykeymaps/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q        , KC_W    , KC_E  , KC_R      , KC_T        ,                               KC_Y          , KC_U       , KC_I      , KC_O   , RCTL_T(KC_P)    ,
     LCTL_T(KC_A), KC_S    , KC_D  , KC_F, KC_G  ,                               KC_H    , KC_J , LT(5,KC_K), KC_L   , KC_ENT          ,
     LSFT_T(KC_Z), KC_X    , KC_C  , LT(2,KC_V)      , LT(3,KC_B)        ,                               LT(3,KC_N)          , LT(2,KC_M)       , KC_COMM   , KC_DOT , RSFT_T(KC_MINS) ,
-    KC_LSFT     , KC_LALT , KC_TAB ,KC_LALT      ,LCMD(KC_SPC) , LT(1,KC_LNG2), LT(1,KC_LNG1), RCMD(KC_SPC)  , _______      , _______     , _______  , KC_RSFT
+    KC_LSFT     , KC_LALT , KC_TAB ,KC_LALT      ,LCMD_T(KC_SPC) , LT(1,KC_LNG2), LT(1,KC_LNG1), RCMD_T(KC_SPC)  , _______      , _______     , _______  , KC_RSFT
   ),
 
   [1] = LAYOUT_universal(
     KC_ESC  , KC_7 , KC_8   , KC_9 , S(KC_EQL) ,                  _______ , KC_HOME , KC_UP   , KC_END  , KC_BSPC ,
     KC_TAB  , KC_4 , KC_5   , KC_6 , KC_MINS   ,                  S(KC_COMM) , KC_LEFT , KC_DOWN , KC_RGHT , KC_DEL  ,
     KC_LSFT , KC_1 , KC_2   , KC_3 , S(KC_8)   ,                  _______ , KC_PGUP , _______   , KC_PGDN , KC_SLSH ,
-    KC_LCAP , KC_0 , KC_SLSH, KC_LALT, _______    , LCMD_T(KC_SPC) , RCMD_T(KC_SPC) , _______ , _______   , _______   , _______   , KC_BSLS
+    KC_LCAP , KC_0 , KC_SLSH, KC_LALT, _______    , LCMD(KC_SPC) , RCMD(KC_SPC) , _______ , _______   , _______   , _______   , KC_BSLS
   ),
 
   [2] = LAYOUT_universal(
